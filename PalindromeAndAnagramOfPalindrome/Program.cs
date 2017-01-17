c98dc9a32c566dd1ce30dcb7757bb7432b2268d2
lawrencesayas <lawrencesayas@yahoo.com>
2017-01-17 13:49:04 +1300

    Add unit test

--- PalindromeAndAnagramOfPalindrome/Program.cs
+++ PalindromeAndAnagramOfPalindrome/Program.cs
@@ -10,6 +10,73 @@ namespace PalindromeAndAnagramOfPalindrome
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Please enter a text: ");
+            string text = Console.ReadLine();
+
+            Word word = new Word(text);
+            word.IdentifyClassificationOfWord();
+            Console.WriteLine(word.Classification);
+
+            Console.ReadLine();
+        }
+    }
+
+    public enum classification { Palindrome, Anagram, Neither }
+
+    public class Word
+    {
+        public string InputString { get; set; }
+        public classification Classification { get; set; }
+
+        public Word(string inputString)
+        {
+            this.InputString = inputString;
+        }
+
+        public bool IdentifyClassificationOfWord()
+        {
+            if (IsPalindrome(0, this.InputString.Length - 1, this.InputString.ToCharArray()) || IsAnagram())
+                return true;
+            else
+            {
+                this.Classification = classification.Neither;
+                return false;
+            }
+        }
+
+        private bool IsAnagram()
+        {
+            //Counts the number of distinct characters
+            var charCount = this.InputString.GroupBy(c => c, (c, i) => new
+            {
+                character = c,
+                count = i.Count()
+            });
+
+            //Checks how many distinct characters are odd in qty
+            if (charCount.Count(c => c.count % 2 == 1) <= 1)
+            {
+                this.Classification = classification.Anagram;
+                return true;
+            }
+            else
+                return false;
+        }
+
+        //Recursion checking whether a word is a palindrome
+        private bool IsPalindrome(int left, int right, char[] inputString)
+        {
+            //When checking reaches the center of the word the word is then a Palidrome
+            if (left == right || left > right)
+            {
+                this.Classification = classification.Palindrome;
+                return true;
+            }
+            //Checks the equality of the left and right side of the word
+            if (inputString[left] == inputString[right])
+                return IsPalindrome(++left, --right, inputString);
+            else
+                return false;
         }
     }
 }
---
+++ UnitTestPalindromeAndAnagramOfPalindrome/UnitTest1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PalindromeAndAnagramOfPalindrome;
+
+namespace UnitTestPalindromeAndAnagramOfPalindrome
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestIdentifyClassificationOfWord_anna_ReturnsTrue()
+        {
+            Word word = new Word("anna");
+            var result = word.IdentifyClassificationOfWord();
+
+            Assert.AreEqual(true, result);
+        }
+
+        [TestMethod]
+        public void TestIdentifyClassificationOfWord_aaaaaaaa_ReturnsTrue()
+        {
+            Word word = new Word("aaaaaaaa");
+            var result = word.IdentifyClassificationOfWord();
+
+            Assert.AreEqual(true, result);
+        }
+
+        [TestMethod]
+        public void TestIdentifyClassificationOfWord_aaaaaaaab_ReturnsTrue()
+        {
+            Word word = new Word("aaaaaaaab");
+            var result = word.IdentifyClassificationOfWord();
+
+            Assert.AreEqual(true, result);
+        }
+
+        [TestMethod]
+        public void TestIdentifyClassificationOfWord_caaaaaaaab_ReturnsFalse()
+        {
+            Word word = new Word("caaaaaaaab");
+            var result = word.IdentifyClassificationOfWord();
+
+            Assert.AreEqual(false, result);
+        }
+
+        [TestMethod]
+        public void TestIdentifyClassificationOfWord_donotbobtonod_ReturnsTrue()
+        {
+            Word word = new Word("donotbobtonod");
+            var result = word.IdentifyClassificationOfWord();
+
+            Assert.AreEqual(true, result);
+        }
+
+        [TestMethod]
+        public void TestIdentifyClassificationOfWord_owefhijpfwai_ReturnsFalse()
+        {
+            Word word = new Word("owefhijpfwai");
+            var result = word.IdentifyClassificationOfWord();
+
+            Assert.AreEqual(false, result);
+        }
+
+        [TestMethod]
+        public void TestIdentifyClassificationOfWord_igdesgide_ReturnsTrue()
+        {
+            Word word = new Word("igdedgide");
+            var result = word.IdentifyClassificationOfWord();
+
+            Assert.AreEqual(true, result);
+        }
+    }
+}
